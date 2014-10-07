da660dcfe4900db288c4d93c848bee701aca438a
bitterulf <bitterulf@users.noreply.github.com>
2014-10-07 11:05:32 +0200

    Set dataManager on joist creation because exactly one is needed.

--- examples/repl/index.js
+++ examples/repl/index.js
@@ -4,10 +4,9 @@ var Joist = require('../../lib');
 var dep = {};
 var data = {};
 
-var joist = Joist.create(dep, data);
+var joist = Joist.create(Joist.DataManager, dep, data);
 
 joist
-  .addDataManager(Joist.DataManager)
   .addSignalSource(Joist.ReplSignal)
   .addAction(require('./logic/loginAction'))
   .addReaction(require('./logic/loginReaction'))
