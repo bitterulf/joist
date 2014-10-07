8f11aa40eb47481aa9aaf174916e6961a65f1dfc
bitterulf <bitterulf@users.noreply.github.com>
2014-10-07 10:40:05 +0200

    Add changeset filter to help with handling changes to the client data.

--- examples/repl/logic/loginReaction.js
+++ examples/repl/logic/loginReaction.js
@@ -4,22 +4,21 @@ var _ = require('underscore');
 module.exports = new Joist.Logic({
   name: 'loginChanged',
   check: function (changes, executed) {
-    var result = false;
-    _.each(changes, function (change) {
-      if (change.path.join(',') == ['login'].join(',')) {
-        result = true;
-      }
-    });
+    var matchedChanges = Joist.filter.matchPath(changes, ['login']);
 
-    return result;
+    return matchedChanges.length;
   },
   command: function (dep, changes, executed, result) {
     result.displayText = [];
-    _.each(changes, function (change) {
-      if (change.path.join(',') == ['login'].join(',')) {
-        result.displayText.push({target: 'info', text: 'you are logged in as ' + change.rhs});
-      }
+
+    var matchedChanges = Joist.filter.matchPath(changes, ['login']);
+    _.each(matchedChanges, function (matchedChange) {
+      result.displayText.push({
+        target: 'info',
+        text: 'you are logged in as ' + matchedChange.rhs
+      });
     });
+
     return result;
   }
 });
