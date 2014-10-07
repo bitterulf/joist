806f33c81d47538e1f253f2a943f248d2c3cc0f4
bitterulf <bitterulf@users.noreply.github.com>
2014-10-07 10:58:37 +0200

    Add demonstration how to integrate server interaction to the actions.

--- examples/browser/logic/loginAction.js
+++ examples/browser/logic/loginAction.js
@@ -1,9 +1,15 @@
+var fakeRequest = function(data, cb) {
+  cb(data.value);
+};
+
 loginAction = new Joist.Logic({
   name: 'login',
   check: function (signal, executed) {
     return (signal.target == 'login');
   },
   command: function (dep, signal, executed) {
-    dep.joist.patch('login', signal.data.value);
+    fakeRequest(signal.data, function(result) {
+      dep.joist.patch('login', result);
+    });
   }
 });
--- examples/repl/logic/loginAction.js
+++ examples/repl/logic/loginAction.js
@@ -1,11 +1,18 @@
 var Joist = require('../../../lib');
 
+// this is how you could integrate server interaction
+var fakeRequest = function(data, cb) {
+  cb(data.value);
+};
+
 module.exports = new Joist.Logic({
   name: 'login',
   check: function (signal, executed) {
     return (signal.target == 'login');
   },
   command: function (dep, signal, executed) {
-    dep.joist.patch('login', signal.data.value);
+    fakeRequest(signal.data, function(result) {
+      dep.joist.patch('login', result);
+    });
   }
 });
